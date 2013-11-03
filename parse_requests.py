274eecb6f1350198546a6eee5bf1fc408b4336be
Quentin Kaiser <kaiserquentin@gmail.com>
2013-11-03 15:48:04 +0100

    Update oktober

--- parse_requests.py
+++ parse_requests.py
@@ -1,1 +1,48 @@
 __author__ = 'quentin'
+
+import MySQLdb
+import sys
+from user_agents import parse
+import json
+
+if __name__ == "__main__":
+    con = None
+    cursor = None
+    try:
+        con = MySQLdb.connect('localhost', 'root', 'my8na6xe', 'beroads', charset='utf8')
+        cursor = con.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute("SELECT COUNT(*) as total FROM requests")
+        total = cursor.fetchone()['total']
+
+        print "Total : %d"%total
+        f = 0
+        t = 30
+        while t < total:
+            query = "SELECT * FROM requests LIMIT %d, %d;"%(f,t)
+            print query
+            cursor.execute(query)
+            requests = cursor.fetchall()
+            for request in requests:
+                user_agent = parse(request['user_agent'])
+                update_query = "UPDATE requests SET browser = '%s', os = '%s', device = '%s' WHERE id = %d"%(user_agent.browser.family, user_agent.os.family, user_agent.device.family, request['id'])
+                cursor.execute(update_query)
+            f+=30
+            t+=30
+
+    except KeyboardInterrupt:
+        if con:
+            if cursor:
+                cursor.close()
+            con.close()
+        sys.exit(2)
+    except MySQLdb.Error as e:
+        print e.message
+        if con:
+            if cursor:
+                cursor.close()
+            con.close()
+        sys.exit(2)
+    except Exception as e:
+        print e.message
+        if con:
+            con.close()
