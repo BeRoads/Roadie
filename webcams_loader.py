44a76da6f07dc2f07ba2851a13428757b6d75811
Quentin Kaiser <kaiserquentin@gmail.com>
2014-01-23 00:40:55 +0100

    The previous fix was setting everything to true. Let's roll back

--- webcams_loader.py
+++ webcams_loader.py
@@ -180,6 +180,10 @@ class WebcamsLoader:
                     return False
 
             im = Image.open(item['output_url'])
+            try:
+                im.load()
+            except IOError:     #this happens when we receive truncated images
+                return True
             R, G, B = im.convert('RGB').split()
             r = R.load()
             g = G.load()
@@ -195,8 +199,6 @@ class WebcamsLoader:
                 return False
         except Exception as e:
             self.logger.exception(e)
-        finally:
-            return True
 
     def scrap_webcams(self, webcams_urls_queue):
         try:
